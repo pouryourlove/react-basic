fe557cdc694ec33643f6be9e7994094d769c6855
pouryourlove <lqu134@naver.com>
2024-07-29 02:23:20 +0900

    react basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <section>Seciton tag</section>
+    <article>article tag</article>
+  </div>
+  )
 }
 
 export default App;
+
+
+
+
+
+// import { Component } from "react";
+// class App extends Component{
+//  To show content on the screen 
+//  render(){
+//     return <h1>Hello Lingling</h1>
+//   }
+// }
+
+
+
+
+
+//components
+//components are independent and reusable bits of code. They serve the same purpose as Javascript functions, but work in isolation and return HTML
+
+//JSX
+//JSX allows us to write HTML in React. JSX makes it easier to write and add HTML in React.
