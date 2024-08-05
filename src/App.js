54ac2b0bb928b49b1fad1d100ad89b10ca75411f
pouryourlove <lqu134@naver.com>
2024-08-05 12:25:47 +0900

    react icons

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
-import './index.css'
-import Button from 'react-bootstrap/Button';
-
-function Buttons() {
-  return (
-    <>
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="success">Success</Button>
-      <Button variant="warning">Warning</Button>
-      <Button variant="danger">Danger</Button>
-      <Button variant="info">Info</Button>
-      <Button variant="light">Light</Button>
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
-    </>
-  );
-}
+import { FaCartArrowDown, FaGithub } from "react-icons/fa";
 
 
 function App() {
   return (
     <>
-      <Buttons/>
+    <FaCartArrowDown/>
+    < FaGithub/>
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+// import './index.css'
+// import Button from 'react-bootstrap/Button';
+
+// function Buttons() {
+//   return (
+//     <>
+//       <Button variant="primary">Primary</Button>
+//       <Button variant="secondary">Secondary</Button>
+//       <Button variant="success">Success</Button>
+//       <Button variant="warning">Warning</Button>
+//       <Button variant="danger">Danger</Button>
+//       <Button variant="info">Info</Button>
+//       <Button variant="light">Light</Button>
+//       <Button variant="dark">Dark</Button>
+//       <Button variant="link">Link</Button>
+//     </>
+//   );
+// }
+
+
+// function App() {
+//   return (
+//     <>
+//       <Buttons/>
+//     </>
+//   )
+// }
+
+// export default App
 
 
 
